77207e68a401c4112b00316f9ec5def2e9f78c29
Danny944 <fadehandaniel2006@gmail.com>
2024-01-24 20:13:13 +0100

    ---

--- src/utils/refreshtoken.js
+++ src/utils/refreshtoken.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+//Handle sessions
 export function generateRefreshToken(id) {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "3d",
