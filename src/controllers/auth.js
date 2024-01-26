a15e2db853df7916bf7fbfa9d780a9ca88e268fe
Danny944 <fadehandaniel2006@gmail.com>
2024-01-26 10:51:06 +0100

    at work

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,10 @@
 import { customer } from "../models2/customer";
 import asyncHandler from "express-async-handler";
 
+const checkIfCustomerExists = async (email) => {};
+
 export const createCustomer = asyncHandler(async (req, res) => {
   const { error, value } = createProfileSchema.validate(payload);
   const { name, email, password, address, phone } = req.body;
+  const checkIfCustomerExists = await customer.findAll();
 });
